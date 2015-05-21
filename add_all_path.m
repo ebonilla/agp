f86cbd45701db0aaf9240b0efd80123c7af11a61
Edwin Bonilla <edwinbonilla@gmail.com>
2015-05-21 22:35:05 +1000

    made sure I am using Trung's utils instead of mine due to differnt assumptions about cholesky

--- add_all_path.m
+++ add_all_path.m
@@ -1,7 +1,4 @@
+% I need to use Trung's utils as my utils 
+% assume cholesky factorisation is L L^T instead of L^T L
 addpath(genpath('./src'), '-begin');
-addpath(genpath('./libs/UGM'), '-begin');
-addpath(genpath('./libs/util'), '-begin');
-%addpath(genpath('./libs/gpml-matlab-v2'), '-begin');
-addpath(genpath('~/Dropbox/Matlab/gpml-matlab-v3.2-2013-01-15'), '-begin');
-addpath(genpath('~/Dropbox/Matlab/utils'), '-begin');
-addpath(genpath('~/Dropbox/Matlab/minFunc_2012'), '-begin');
+addpath(genpath('./libs'), '-begin');
